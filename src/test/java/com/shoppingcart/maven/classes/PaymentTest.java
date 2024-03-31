87c0cee5d040cd62907cc96006811346358c4a28
niloofarmansoor <niloofar@Niloofars-MBP-2.lan>
2024-03-31 15:55:41 -0500

    added some payment tests

--- src/test/java/com/shoppingcart/maven/classes/PaymentTest.java
+++ src/test/java/com/shoppingcart/maven/classes/PaymentTest.java
@@ -21,6 +21,17 @@ public class PaymentTest {
     }
 
     @Test
+    public void testProcessPaymentFailure2() {
+        String message = Payment.processPayment(0.0, "MasterCard", "12345", "123", "01/25");
+        assertEquals("Payment amount is invalid.", message);
+    }
+
+    public void testProcessPaymentFailure3() {
+        String message = Payment.processPayment(55.0, "MasterCard", "12345", "123", "02/23");
+        assertEquals("Payment rejected due to wrong card information.", message);
+    }
+
+    @Test
     public void testCheckCard() {
         assertTrue(Payment.checkCard("Visa", "1234567890123456", "123", "01/25"));
         assertFalse(Payment.checkCard("MasterCard", "12345", "123", "01/25"));
